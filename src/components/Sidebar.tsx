9b89fa230d2016e4b64ea853dec69b812e78fd63
HevertonL <hevertonluizs@gmail.com>
2022-11-19 00:29:52 -0300

    Projeto Completo

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,28 @@
+import {PencilLine} from 'phosphor-react'
+import { Avatar } from './Avatar'
+import styles from "./Sidebar.module.css"
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+
+      <img className={styles.cover} 
+      src="https://images.unsplash.com/photo-1630710478039-9c680b99f800?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
+      
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/HevertonL.png"/>
+        <strong>Heverton Luiz</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        
+        <a href="#"> 
+        <PencilLine size={20}/>
+        Editar seu perfil
+        </a>
+
+      </footer>
+    </aside>
+  )
+} 
